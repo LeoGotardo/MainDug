dd76e67029f0541b8daf476a6a087679a2514731
Leonardo Gotardo <125305856+LeoGotardo@users.noreply.github.com>
2024-03-19 12:37:54 -0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,62 +1,152 @@
-
 from model import Model, Generator
 
 class Controller:
     def __init__(self):
-        # Initialize a Model object in the Controller instance
         self.model = Model()
-        self.Generator = Generator()
+        self.generator = Generator()
 
-    def add_user(self, login, password, password_confirm):
-        # Calls the add_user method of Model and returns the result
-        if self.model.is_credential_valid(login, password, password_confirm) == True:
-            if type(self.model.add_user(login, password)) != str :
+    def add_user(self, login: str, password: str, password_confirm: str) -> bool:
+        """Adds a new user if the credentials are valid and unique.
+        
+        Args:
+            login: User's login name.
+            password: User's password.
+            password_confirm: Confirmation of the user's password.
+
+        Returns:
+            True if the user was added successfully, otherwise returns an error message.
+        """
+        if self.model.is_credential_valid(login, password, password_confirm):
+            user_add_result = self.model.add_user(login, password)
+            if type(user_add_result) != str:
                 return True
             else:
-                return self.model.add_user(login, password)
+                return user_add_result
         else:
             return self.model.is_credential_valid(login, password, password_confirm)
+        
+
+    def is_login_valid(self, login: str, password: str) -> bool:
+        """Checks if login credentials are valid.
+        
+        Args:
+            login: User's login name.
+            password: User's password.
+
+        Returns:
+            True if the login credentials are valid, False otherwise.
+        """
+        return self.model.is_login_valid(login, password)
 
 
-    def is_login_valid(self, login, Password):
-        # Calls the is_login_valid method of Model and returns the result
-        return self.model.is_login_valid(login, Password)
+    def update_user(self, user_id, parameter: str, new_value: str) -> bool:
+        """Updates a user's information.
+        
+        Args:
+            user_id: The ID of the user to update.
+            parameter: The field to update.
+            new_value: The new value for the field.
+
+        Returns:
+            True if the update was successful, False otherwise.
+        """
+        return self.model.update_user(user_id, parameter, new_value)
     
 
-    def update_user(self, user_id, parameter, new_value):
-        # Calls the update_user method of Model and returns the result
-        return self.model.update_user(user_id, parameter, new_value)
+    def delete_user(self, user_id) -> bool:
+        """Deletes a user.
+        
+        Args:
+            user_id: The ID of the user to delete.
 
-
-    def delete_user(self, user_id):
-        # Calls the delete_user method of Model and returns the result
+        Returns:
+            True if the user was successfully deleted, False otherwise.
+        """
         return self.model.delete_user(user_id)
+    
 
+    def find_user_id(self, login: str, password: str) -> int:
+        """Finds a user's ID based on login credentials.
+        
+        Args:
+            login: User's login name.
+            password: User's password.
 
-    def find_user_id(self, login, password):
-        # Calls the find_user_id method of Model and returns the user ID
+        Returns:
+            The user's ID if found, otherwise returns None.
+        """
         return self.model.find_user_id(login, password)
     
 
-    def findPasswords(self,id):
-        return self.model.findPasswords(id)
+    def find_passwords(self, user_id) -> list:
+        """Finds all passwords associated with a user.
+        
+        Args:
+            user_id: The ID of the user.
+
+        Returns:
+            A list of passwords associated with the user.
+        """
+        return self.model.find_passwords(user_id)
     
 
-    def delete_item(self, id, item_id):
-        return self.model.delete_item(id, item_id)
+    def delete_item(self, user_id, item_id) -> bool:
+        """Deletes an item for a user.
+        
+        Args:
+            user_id: The ID of the user.
+            item_id: The ID of the item to delete.
+
+        Returns:
+            True if the item was successfully deleted, False otherwise.
+        """
+        return self.model.delete_item(user_id, item_id)
     
 
-    def addNewLog(self, id, site, login, password):
-        return self.model.addNewLog(id, site, login, password)
+    def add_new_log(self, user_id, site: str, login: str, password: str) -> bool:
+        """Adds a new log for a user.
+        
+        Args:
+            user_id: The ID of the user.
+            site: The site associated with the log.
+            login: The login name for the site.
+            password: The password for the site.
 
-
-    def editLog(self, paramter, user_id, id, newLog):
-        return self.model.editLog(paramter, user_id, id, newLog)
+        Returns:
+            True if the log was successfully added, False otherwise.
+        """
+        return self.model.add_new_log(user_id, site, login, password)
     
-    def generator(self, number, lower, symbol, upper, len):
-        return self.Generator.generator(number, lower, symbol, upper, len)
 
+    def edit_log(self, parameter: str, user_id, log_id, new_log: str) -> bool:
+        """Edits a log for a user.
+        
+        Args:
+            parameter: The parameter to edit in the log.
+            user_id: The ID of the user.
+            log_id: The ID of the log to edit.
+            new_log: The new value for the log.
+
+        Returns:
+            True if the log was successfully edited, False otherwise.
+        """
+        return self.model.edit_log(parameter, user_id, log_id, new_log)
+    
+    
+    def generate_password(self, number: bool, lower: bool, symbol: bool, upper: bool, length: int) -> str:
+        """Generates a password based on specified criteria.
+        
+        Args:
+            number: Include numbers in the password.
+            lower: Include lowercase letters.
+            symbol: Include symbols.
+            upper: Include uppercase letters.
+            length: The length of the password.
+
+        Returns:
+            The generated password.
+        """
+        return self.generator.generate(number, lower, symbol, upper, length)
 
 if __name__ == "__main__":
-    # Create a Controller instance to test the class
     controller = Controller()
